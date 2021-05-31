cef9b77e7c0a42ed8af2032135636f16715b8508
Shambonik <shambonik@gmail.com>
2021-05-31 03:51:19 +0300

    practice 21 is not practice 23 now

--- practice_21/src/main/java/com/example/practice14/repositories/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.practice14.repositories;
-
-import com.example.practice14.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
